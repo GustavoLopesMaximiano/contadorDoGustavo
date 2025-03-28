46bdf95a25f137006f49df88241df5174230cde2
GustavoLopesMaximiano <gustavo.maximiano.ifc@gmail.com>
2025-03-28 08:29:47 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+
+const contador = ref(0)
+
+function incrementar() {
+  contador.value++
+}
+
+function decrementar() {
+  contador.value--
+}
+</script>
+
+<template>
+  <h1>contador</h1>
+  <p>Contador: {{ contador }}</p>
+  <button @click="incrementar" class="botao1">+</button>
+  <button @click="decrementar" class="botao1">-</button>
+</template>
+
+<style scoped>
+  
+</style>
